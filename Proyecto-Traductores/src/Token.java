d3be363d1f0a1cbeaf408cae1e1d26e57ce1700d
Ricardo Garcia <ricardogarciar2102@gmail.com>
2026-05-23 19:17:49 -0400

    Subida de jflex-full-1.7.0.jar

--- Proyecto-Traductores/src/Token.java
+++ Proyecto-Traductores/src/Token.java
@@ -2,7 +2,9 @@ public class Token {
     private TokenConstants tipo;
     private String lexema;
     public Token(TokenConstants tipo, String lexema){
-        this.tipo = tipo;
-        this.lexema = lexema;
+
+        this.tipo = tipo; //tipo del token, de los enumerados en TokenConstants
+        this.lexema = lexema; //la cadena que reconoce el token 
     } 
 }
+
