61805f61722700d7774c22ed9479408c54ae7de9
dagragirena <dagragirena@gmail.com>
2026-05-23 18:11:57 -0400

    Actualizacion

---
+++ Proyecto-Traductores/src/Token.java
@@ -0,0 +1,8 @@
+public class Token {
+    private TokenConstants tipo;
+    private String lexema;
+    public Token(TokenConstants tipo, String lexema){
+        this.tipo = tipo;
+        this.lexema = lexema;
+    } 
+}
